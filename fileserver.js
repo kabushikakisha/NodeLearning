940afd268b5efe0c8b3014bf4cc55660746ef264
kabushikakisha <37305991+kabushikakisha@users.noreply.github.com>
2018-05-19 15:35:49 -0500

    finished fileserver

--- fileserver.js
+++ fileserver.js
@@ -9,8 +9,24 @@ http.createServer(function(req, res) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(html);
         });
+    } else if (req.url.match(/.css$/)) {
+        var cssPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+
+        res.writeHead(200, {'Content-Type': 'text/css'});
+
+        fileStream.pipe(res);
+    } else if (req.url.match(/.jpg$/)) {
+        var imgPath = path.join(__dirname, 'public', req.url);
+        var imgStream = fs.createReadStream(imgPath);
+
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+
+        imgStream.pipe(res);
     } else {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.end('404 File Not Found');
     }
 }).listen(3000);
+
+console.log('File server running on port 3000.\nPress "CTRL" + "C" to quit.');
